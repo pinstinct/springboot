6fef26d23907ca0bcd7bdf97b5dd0dc680b5d02f
pinstinct <gusals3407@gmail.com>
2021-07-09 13:39:42 +0900

    Getting started

---
+++ src/main/java/com/limhm/springboot/controller/GreetingController.java
@@ -0,0 +1,24 @@
+package com.limhm.springboot.controller;
+
+import com.limhm.springboot.dto.GreetingDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+    /*
+    * Building a RESTful Web Service
+    * https://spring.io/guides/gs/rest-service/
+    * */
+
+    private static final String template = "Hello, %s";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public GreetingDto greeting(@RequestParam(value = "name", defaultValue = "World") String name){
+        return new GreetingDto(counter.incrementAndGet(), String.format(template, name));
+    }
+}
